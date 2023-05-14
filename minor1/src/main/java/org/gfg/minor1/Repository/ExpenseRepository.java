fb9d77620811c3733ea9dcf4c15f44af615d0624
Kajuglotra <51613822+Kajuglotra@users.noreply.github.com>
2023-05-14 17:30:49 +0530

    adding new files after this

--- minor1/src/main/java/org/gfg/minor1/Repository/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package org.gfg.minor1.Repository;
-
-import org.gfg.minor1.model.ExpenseDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseRepository extends JpaRepository<ExpenseDetails, Integer> {
-
-    public ExpenseDetails findByExpenseType(String type);
-}
--- minor1/src/main/java/org/gfg/minor1/Repository/TxnRepository.java
+++
@@ -1,7 +0,0 @@
-package org.gfg.minor1.Repository;
-
-import org.gfg.minor1.model.TransactionDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TxnRepository extends JpaRepository<TransactionDetails, Integer> {
-}
